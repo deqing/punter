535e19682b2eda8cca4f4fb4aa93789db91c9502
Deqing Huang <hdqdevin@gmail.com>
2017-11-22 05:18:42 +0000

    email fix

--- sendemail.sh
+++ sendemail.sh
@@ -1,1 +1,1 @@
-mail -a "Content-type: text/html" -s $(cat output_title.txt) khingblue@gmail.com < output.html
+mail -a "Content-type: text/html" -s $(cat output_title.txt output_empty_pickles.txt) khingblue@gmail.com < output.html
