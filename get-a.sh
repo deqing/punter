e0b17f29a87b9c1d91a1066e98cc6d741cc17a6c
Deqing Huang <hdqdevin@gmail.com>
2017-11-22 03:43:43 +0000

    send email

--- get-a.sh
+++ get-a.sh
@@ -7,3 +7,5 @@ sleep 10
 python3 punter.py $AU_ONLY --a
 sudo systemctl stop tunnelbear@oz
 python3 punter.py $US_SAFE,$AU_ONLY --a --print-only
+
+bash sendemail.sh
---
+++ sendemail.sh
@@ -0,0 +1,1 @@
+mail -a "Content-type: text/html" -s $(cat output_title.txt) khingblue@gmail.com < output.html
