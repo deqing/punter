1769f90850c18bf70d4cd3d2ee2b4d8949ad0ad9
Deqing Huang <hdqdevin@gmail.com>
2017-11-23 05:32:00 +0000

    fix au only problem

--- au-only.sh
+++ au-only.sh
@@ -2,6 +2,9 @@ sudo systemctl start tunnelbear@oz
 sleep 10
 for param
 do
-  python3 punter.py $param --a --eng --ita --liga --get-only
+  python3 punter.py $param --a --get-only
+  python3 punter.py $param --eng --get-only
+  python3 punter.py $param --ita --get-only
+  python3 punter.py $param --liga --get-only
 done
 sudo systemctl stop tunnelbear@oz
--- get-all.sh
+++ get-all.sh
@@ -1,8 +1,8 @@
 US_SAFE="bet365,betstar,ladbrokes,madbookie,palmerbet,topbetta,ubet,unibet"
-AU_ONLY="luxbet,crownbet,sportsbet,tab,williamhill"
-
 python3 punter.py $US_SAFE --a --eng --ita --liga --get-only
+
 bash au-only.sh tab williamhill crownbet luxbet sportsbet
+
 python3 punter.py all --a --eng --ita --liga --print-only
 
 bash sendemail.sh
