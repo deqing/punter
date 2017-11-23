b2a9fff43681cb1a6adaca5e522744ec6bea7d7d
Deqing Huang <hdqdevin@gmail.com>
2017-11-23 06:21:15 +0000

    au-only: trying fix

--- au-only.sh
+++ au-only.sh
@@ -3,8 +3,20 @@ sleep 10
 for param
 do
   python3 punter.py $param --a --get-only
+done
+
+for param
+do
   python3 punter.py $param --eng --get-only
+done
+
+for param
+do
   python3 punter.py $param --ita --get-only
+done
+
+for param
+do
   python3 punter.py $param --liga --get-only
 done
 sudo systemctl stop tunnelbear@oz
