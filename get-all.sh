9cf747ca35e0f715ffeb6a7aee56c5a9858cae68
Deqing Huang <hdqdevin@gmail.com>
2017-11-23 05:55:48 +0000

    AU-only: Try another way

--- get-all.sh
+++ get-all.sh
@@ -1,8 +1,7 @@
-US_SAFE="bet365,betstar,ladbrokes,madbookie,palmerbet,topbetta,ubet,unibet"
-python3 punter.py $US_SAFE --a --eng --ita --liga --get-only
-
 bash au-only.sh tab williamhill crownbet luxbet sportsbet
 
+US_SAFE="bet365,betstar,ladbrokes,madbookie,palmerbet,topbetta,ubet,unibet"
+python3 punter.py $US_SAFE --a --eng --ita --liga --get-only
 python3 punter.py all --a --eng --ita --liga --print-only
 
 bash sendemail.sh
