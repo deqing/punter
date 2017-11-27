ede79ba345a2d856eb3472bfd949e8c9223c59cc
Deqing Huang <hdqdevin@gmail.com>
2017-11-27 23:59:03 +0000

    add aws

---
+++ aws.sh
@@ -0,0 +1,1 @@
+python3 punter.py betstar,crownbet,ladbrokes,luxbet,madbookie,palmerbet,pinnacle,sportsbet,tab,topbetta,ubet,unibet,williamhill --a --arg --eng --ita --liga --send-email-when-found --loop
