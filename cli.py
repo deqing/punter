3249d05dcfa17e8fe34b4021db55c0414f63e22c
Deqing Huang <hdqdevin@gmail.com>
2017-11-28 23:27:53 +0000

    Separate cli and core

---
+++ cli.py
@@ -0,0 +1,59 @@
+import signal
+import sys
+from docopt import docopt
+
+from .core import WebWorker
+
+
+def main():
+    """Punter command-line interface.
+
+    Usage:
+      cli.py <websites> [options]
+
+    Options:
+      --all               Get all leagues
+      --a                 Get A-league
+      --arg               Get Argentina league
+      --eng               Get EPL
+      --ita               Get Italy league
+      --liga              Get La Liga
+      --get-only          Don't merge and print matches
+      --print             Don't get latest odds, just print out based on saved odds
+      --send-email-api    Send email by MailGun's restful api
+      --send-email-smtp   Send email by SMTP (note: not working in GCE)
+      --send-email-when-found    Send email by api when returns bigger than 99.5
+      --loop              Repeat every 5 mins
+
+    Example:
+      cli.py luxbet,crownbet --a
+      cli.py all --eng
+    """
+    worker = False
+
+    def signal_handler(_, __):
+        print('You pressed Ctrl+C!')
+        if worker and worker.driver:
+            worker.driver.quit()
+        sys.exit(0)
+    signal.signal(signal.SIGINT, signal_handler)
+
+    args = docopt(str(main.__doc__))
+    worker = WebWorker(is_get_data=not args['--print'])
+    worker.run(
+        websites=args['<websites>']
+        , is_get_a=args['--a']
+        , is_get_arg=args['--arg']
+        , is_get_eng=args['--eng']
+        , is_get_ita=args['--ita']
+        , is_get_liga=args['--liga']
+        , is_get_only=args['--get-only']
+        , is_send_email_api=args['--send-email-api']
+        , is_send_email_smtp=args['--send-email-smtp']
+        , is_send_email_when_found=args['--send-email-when-found']
+        , is_loop=args['--loop']
+    )
+
+
+if __name__ == "__main__":
+    main()
--- test.py
+++
@@ -1,8 +0,0 @@
-def foo(a=(1,2,3)):
-    if 1 in a:
-        print('ok')
-    else:
-        print('no')
-
-
-foo(a=[4,1])
