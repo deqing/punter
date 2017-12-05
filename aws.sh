909752407c4c2fbba2588be94a6514993c42b69c
Deqing Huang <hdqdevin@gmail.com>
2017-12-05 22:33:35 +0000

    update aws

--- aws.sh
+++ aws.sh
@@ -1,1 +1,1 @@
-python3 -u cli.py all --a --arg --eng --ita --liga --w --send-email-when-found --loop=1 --ask-gce=bet365,bluebet,ladbrokes,madbookie,palmerbet,ubet,unibet --gce-ip=
+python3 -u cli.py all a,arg,eng,gem,ita,liga,uefa,w --send-email-when-found --loop=1 --ask-gce=bet365,bluebet,ladbrokes,madbookie,palmerbet,ubet,unibet --gce-ip=
