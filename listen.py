08ae024fba708aa5faf176fa0025ef79e6d2a561
Deqing Huang <hdqdevin@gmail.com>
2018-05-21 06:43:12 +0100

    add multi files to listen.py

--- listen.py
+++ listen.py
@@ -8,8 +8,8 @@ from worker import WebWorker
 from werkzeug.utils import secure_filename
 
 # python -u C:\Users\devin.huang\OneDrive\Documents\sty\punter\listen.py
-#UPLOAD_FOLDER = 'C:\\hdq\\tmp'
-UPLOAD_FOLDER = '/home/ubuntu/snj/'  # FIXME: should be an better path like: gettempdir()
+UPLOAD_FOLDER = 'C:\\hdq\\tmp'
+#UPLOAD_FOLDER = '/home/ubuntu/snj/'  # FIXME: should be an better path like: gettempdir()
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -48,26 +48,6 @@ def ping():
     return 'yes'
 
 
-@app.route("/upload", methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file:
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('index'))
-    return """
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    <p>%s</p>
-    """ % "<br>".join(os.listdir(app.config['UPLOAD_FOLDER'],))
-
-
 @app.route("/ifttt", methods=['GET', 'POST'])
 def ifttt():
     with open('ifttt.log', 'a') as f:
@@ -75,55 +55,52 @@ def ifttt():
     return 'ok'
 
 
-@app.route("/abc", methods=['GET', 'POST'])
-def abc():
+def html_template(s):
+    return """
+    <!doctype html>
+    <form action="" method=post enctype=multipart/form-data>
+        {}
+        <input type=submit value=Upload>
+    </form>     
+    <br>Current files:
+    <ul><li>{}</li></ul>
+    """.format(s, "</li><li>".join(os.listdir(app.config['UPLOAD_FOLDER'],)))
+
+
+@app.route("/dqfile", methods=['GET', 'POST'])
+def dqfile():
     if request.method == 'POST':
         for n in range(15):
-            fn = 'f' + str(n+1)
-            cn = 'content' + str(n+1)
+            fn = 'f' + str(n)
+            file = request.files[fn]
+            if file:
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('dqfile'))
+
+    s = ''
+    for i in range(15):
+        s += '<input type=file name="f{}"><br>'.format(i)
+    return html_template(s)
+
+
+@app.route("/dqtext", methods=['GET', 'POST'])
+def dqtext():
+    if request.method == 'POST':
+        for n in range(15):
+            fn = 'f' + str(n)
+            cn = 'content' + str(n)
             filename = request.form[fn]
             if filename:
                 with open(os.path.join(app.config['UPLOAD_FOLDER'], filename), 'a') as f:
                     f.write(request.form[cn])
+        return redirect(url_for('dqtext'))
 
-    return """
-    <!doctype html>
-    <form action="" method=post>
-        <textarea name="content1" rows="10" cols="50" /></textarea><br>
-        filename: <input type="text" name="f1"><br>
-        <textarea name="content2" rows="10" cols="50" /></textarea><br>
-        filename: <input type="text" name="f2"><br>
-        <textarea name="content3" rows="10" cols="50" /></textarea><br>
-        filename: <input type="text" name="f3"><br>
-        <textarea name="content4" rows="10" cols="50" /></textarea><br>
-        filename: <input type="text" name="f4"><br>
-        <textarea name="content5" rows="10" cols="50" /></textarea><br>
-        filename: <input type="text" name="f5"><br>
-        <textarea name="content6" rows="10" cols="50" /></textarea><br>
-        filename: <input type="text" name="f6"><br>
-        <textarea name="content7" rows="10" cols="50" /></textarea><br>
-        filename: <input type="text" name="f7"><br>
-        <textarea name="content8" rows="10" cols="50" /></textarea><br>
-        filename: <input type="text" name="f8"><br>
-        <textarea name="content9" rows="10" cols="50" /></textarea><br>
-        filename: <input type="text" name="f9"><br>
-        <textarea name="content10" rows="10" cols="50" /></textarea><br>
-        filename: <input type="text" name="f10"><br>
-        <textarea name="content11" rows="10" cols="50" /></textarea><br>
-        filename: <input type="text" name="f11"><br>
-        <textarea name="content12" rows="10" cols="50" /></textarea><br>
-        filename: <input type="text" name="f12"><br>
-        <textarea name="content13" rows="10" cols="50" /></textarea><br>
-        filename: <input type="text" name="f13"><br>
-        <textarea name="content14" rows="10" cols="50" /></textarea><br>
-        filename: <input type="text" name="f14"><br>
-        <textarea name="content15" rows="10" cols="50" /></textarea><br>
-        filename: <input type="text" name="f15"><br>        
-        <input type=submit value=Save>
-    </form>     
-    <br>Current files:
-    <ul><li>%s</li></ul>
-    """ % "</li><li>".join(os.listdir(app.config['UPLOAD_FOLDER'],))
+    s = ''
+    for i in range(15):
+        s += '<textarea name="content{}" rows="10" cols="50" /></textarea><br>' \
+            'filename: <input type="text" name="f{}"><br>'.format(i, i)
+    return html_template(s)
 
 
 if __name__ == '__main__':
