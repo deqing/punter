2dfe8178fc5e046ab758e535f8f449151ccec699
Deqing Huang <hdqdevin@gmail.com>
2018-05-22 15:27:55 +0100

    add ifttt support for habitica

--- listen.py
+++ listen.py
@@ -1,5 +1,7 @@
 import pickle
+import configparser
 import os
+import requests
 from tempfile import gettempdir
 from flask import Flask
 from flask import jsonify
@@ -96,6 +98,59 @@ def dqtext():
         s += '<textarea name="content{}" rows="10" cols="50" /></textarea><br>' \
             'filename: <input type="text" name="f{}"><br>'.format(i, i)
     return html_template(s)
+
+
+def load_auth(configfile):
+    try:
+        cf = open(configfile)
+    except IOError:
+        print("Unable to find '%s'." % configfile)
+        exit(1)
+
+    config = configparser.ConfigParser({'checklists': False})
+    config.readfp(cf)
+
+    cf.close()
+
+    # Get data from config
+    rv = {}
+    try:
+        rv = {'url': config.get('Habitica', 'url'),
+              'checklists': config.get('Habitica', 'checklists'),
+              'x-api-user': config.get('Habitica', 'login'),
+              'x-api-key': config.get('Habitica', 'password'),
+              'content-type': 'application/json'}
+
+    except configparser.NoSectionError:
+        print("No 'Habitica' section in '%s'" % configfile)
+        exit(1)
+
+    except configparser.NoOptionError as e:
+        print("Missing option in auth file '%s': %s"
+                      % (configfile, e.message))
+        exit(1)
+
+    # Return auth data as a dictionary
+    return rv
+
+
+@app.route("/habitica_update_credit", methods=['GET', 'POST'])
+def timer_habit_update_credit():
+    auth = load_auth('auth.cfg')
+    cmd = request.args.get('cmd')
+
+    url = 'https://habitica.com/api/v3/tasks/28068b64-0105-4ddb-bd78-6651bce8a241/score/'
+    if cmd == 'timer_success':
+        url += 'up'
+    elif cmd == 'timer_failed':
+        url += 'down'
+    elif cmd == 'pomodoro_done':
+        url = 'https://habitica.com/api/v3/tasks/69924582-fdef-470d-a511-18bead518e23/score/up'
+    else:
+        return 'Invalid request'
+
+    res = requests.post(url, headers=auth)
+    return res.text
 
 
 if __name__ == '__main__':
